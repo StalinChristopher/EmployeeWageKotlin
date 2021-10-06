12f93b91a56e27ef5048917742b855ec63d7aadc
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-10-07 01:34:28 +0530

    added method to calculate dailywage of full time and part time employee

--- src/com/employeewage/CheckAttendance.kt
+++ src/com/employeewage/CheckAttendance.kt
@@ -2,10 +2,13 @@ package com.employeewage
 
 object CheckAttendance {
     fun attendance() :Int {
-        var randomNum = (0..1).random()
-        if(randomNum == IS_PRESENT){
-            println("Employee is present")
-            return IS_PRESENT
+        var randomNum = (0..2).random()
+        if(randomNum == FULL_TIME){
+            println("Employee is present and is working full time")
+            return FULL_TIME
+        }else if(randomNum == PART_TIME){
+            println("Employee is present and is working part time")
+            return PART_TIME
         }else{
             println("Employee is absent")
             return IS_ABSENT
--- src/com/employeewage/DailyWage.kt
+++ src/com/employeewage/DailyWage.kt
@@ -4,10 +4,11 @@ import com.employeewage.CheckAttendance.attendance
 
 fun dailyWage(): Int{
     var empAttendance = attendance()
-    if(empAttendance == IS_PRESENT){
-        return DAILY_WORKING_HOURS * WAGE_PER_HOUR
-    }else{
+    if(empAttendance == FULL_TIME){
+        return FULL_TIME_WORKING_HOURS * WAGE_PER_HOUR
+    }else if(empAttendance == PART_TIME){
+        return PART_TIME_WORKING_HOURS * WAGE_PER_HOUR
+    }else {
         return 0
-
     }
 }
