5b00fce9279d738ffc3beeed340a0d5ca28748c0
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-10-10 16:35:10 +0530

    Added procedural way of calculating wage for multiple companies using class method

--- src/com/employeewage/DailyWage.kt
+++ src/com/employeewage/DailyWage.kt
@@ -1,10 +1,10 @@
 package com.employeewage
 
 object DailyWage{
-    fun dailyWage(employeeType: Int): Pair<Int,Int>{
+    fun dailyWage(employeeType: Int, wagePerHour : Int): Pair<Int,Int>{
         var dailyWage = when(employeeType){
-            EmployeeWage.FULL_TIME -> {Pair(EmployeeWage.FULL_TIME_WORKING_HOURS * EmployeeWage.WAGE_PER_HOUR,EmployeeWage.FULL_TIME_WORKING_HOURS)}
-            EmployeeWage.PART_TIME -> {Pair(EmployeeWage.PART_TIME_WORKING_HOURS * EmployeeWage. WAGE_PER_HOUR,EmployeeWage.PART_TIME_WORKING_HOURS)}
+            EmployeeWage.FULL_TIME -> {Pair(EmployeeWage.FULL_TIME_WORKING_HOURS * wagePerHour,EmployeeWage.FULL_TIME_WORKING_HOURS)}
+            EmployeeWage.PART_TIME -> {Pair(EmployeeWage.PART_TIME_WORKING_HOURS * wagePerHour,EmployeeWage.PART_TIME_WORKING_HOURS)}
             else -> {Pair(0,0)}
         }
         return  dailyWage
--- src/com/employeewage/Main.kt
+++ src/com/employeewage/Main.kt
@@ -5,6 +5,7 @@ import com.employeewage.CheckAttendance.attendance
 
 fun main(){
     println("Welcome to employee wage problem")
-    Employee.computeWage()
+    Employee.computeWage("TCS",20,25,90)
+    Employee.computeWage("Suzuki", 25,27,85)
 
 }
