71976f945a200c39d1b69fac5149bf4aa2b43249
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-10-10 12:50:30 +0530

    Added method to calculate wages based on the given condition

--- src/com/employeewage/Main.kt
+++ src/com/employeewage/Main.kt
@@ -2,14 +2,11 @@ package com.main
 
 import com.employeewage.CheckAttendance
 import com.employeewage.CheckAttendance.attendance
+import com.employeewage.conditionalWage
 import com.employeewage.dailyWage
 import com.employeewage.monthWages
 
 fun main(){
     println("Welcome to employee wage problem")
-    var empAttendance = attendance()
-    var dailyWage = dailyWage(empAttendance)
-    println("Today's wage of the employee is $dailyWage")
-    var monthlyWage = monthWages(dailyWage)
-    println("Monthly wage of the employee is $monthlyWage")
+    conditionalWage()
 }
